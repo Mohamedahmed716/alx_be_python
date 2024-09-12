bc217cdd69b4ffc77082fea996238a92180a707d
Mohamedahmed716 <es.Mohamed.nomier2023@alexu.edu.eg>
2024-09-12 19:57:43 +0300

    sa

---
+++ basic_operations.py
@@ -0,0 +1,5 @@
+number1 = 10
+number2 = 5
+print("Addition of", number1 ,"and", number2 ,"is" , number1 + number2)
+print("Subtraction of", number1 ,"and", number2 ,"is" , number1 - number2)
+print("Multipication of", number1 ,"and", number2 ,"is" , number1 * number2)
